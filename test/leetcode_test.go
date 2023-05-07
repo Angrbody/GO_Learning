aeb67cd426922463d4626e437549e27f95ced619
Angrbody <13522017717@163.com>
2023-05-07 21:38:38 +0800

    master->main

--- test/leetcode_test.go
+++ test/leetcode_test.go
@@ -50,44 +50,36 @@ func Swap(x, y int) (int, int) {
 }
 
 func main() {
-	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
-	var res, cur int = 0, 0
-	for _, value := range nums {
-		// 如果加上当前的value还是正的，就延长子数组的长度
-		if cur+value >= 0 {
-			cur += value
-			res = Max(cur, res)
-		}
-
-		// 如果加上当前的value变成负的了，就没有必要继续加
-		if cur+value < 0 {
-			res = Max(cur, res)
-			cur = Max(cur, 0)
-		}
+	points := [][]int{
+		{1, 2},
+		{2, 3},
+		{3, 4},
+		{4, 5},
 	}
-	res = Max(res, cur)
-	fmt.Println(res)
-
-	var a, b int = 1, 2
-	fmt.Printf("swap前的a和b：a = %d, b = %d", a, b)
-	a, b = Swap(a, b)
-	fmt.Printf("swap后的a和b：a = %d, b = %d", a, b)
-
-	sort.Ints(nums)
-	for _, value := range nums {
-		fmt.Println(value, " ")
-	}
-
-	sort.Slice(nums, func(i, j int) bool {
-		tmpI, tmpJ := nums[i], nums[j]
-		if tmpI < 0 {
-			tmpI = -1 * tmpI
+	// 先按start大小排序
+	sort.Slice(points, func(i, j int) bool {
+		if points[i][0] == points[j][0] {
+			return points[i][1] < points[j][1]
 		}
-		if tmpJ < 0 {
-			tmpJ = -1 * tmpJ
-		}
-		return tmpI < tmpJ
+		return points[i][0] < points[j][0]
 	})
+
+	// 计算重叠数量
+	res := 1
+	win := points[0] // 初始窗口
+
+	for i := 1; i < len(points); i++ {
+		// 若不重叠
+		if points[i][0] > points[i-1][1] {
+			win = points[i]
+			res++
+			continue
+		}
+		// 若重叠，更新窗口
+		win[0] = points[i][0]
+	}
+
+	fmt.Print(res)
 }
 
 func FindGas(gas, cost []int) int {
@@ -117,47 +109,47 @@ func FindGas(gas, cost []int) int {
 	return -1
 }
 
-func canCompleteCircuit(gas []int, cost []int) int {
-	work := []int{}
-	for i := range gas {
-		work = append(work, gas[i]-cost[i])
-	}
+// func canCompleteCircuit(gas []int, cost []int) int {
+// 	work := []int{}
+// 	for i := range gas {
+// 		work = append(work, gas[i]-cost[i])
+// 	}
 
-	curSum, totalSum, start := 0, 0, 0
-	for i, v := range work {
-		curSum += v
-		totalSum += v
-		if curSum < 0 {
-			curSum = 0
-			start = i + 1
-		}
-	}
-	if totalSum < 0 {
-		return -1
-	}
-	return start
-}
+// 	curSum, totalSum, start := 0, 0, 0
+// 	for i, v := range work {
+// 		curSum += v
+// 		totalSum += v
+// 		if curSum < 0 {
+// 			curSum = 0
+// 			start = i + 1
+// 		}
+// 	}
+// 	if totalSum < 0 {
+// 		return -1
+// 	}
+// 	return start
+// }
 
-func candy(ratings []int) int {
-	res := len(ratings) // 每人至少一颗
-	add := make([]int, len(ratings))
-	// 从左到右遍历
-	for i := 1; i < len(ratings); i++ {
-		if ratings[i] > ratings[i-1] {
-			add[i] = add[i-1] + 1
-		}
-	}
+// func candy(ratings []int) int {
+// 	res := len(ratings) // 每人至少一颗
+// 	add := make([]int, len(ratings))
+// 	// 从左到右遍历
+// 	for i := 1; i < len(ratings); i++ {
+// 		if ratings[i] > ratings[i-1] {
+// 			add[i] = add[i-1] + 1
+// 		}
+// 	}
 
-	// 从右到左遍历
-	for i := len(ratings) - 2; i > 0; i-- {
-		if ratings[i] > ratings[i+1] && add[i] <= add[i+1] {
-			add[i] = add[i+1] + 1
-		}
-	}
+// 	// 从右到左遍历
+// 	for i := len(ratings) - 2; i > 0; i-- {
+// 		if ratings[i] > ratings[i+1] && add[i] <= add[i+1] {
+// 			add[i] = add[i+1] + 1
+// 		}
+// 	}
 
-	for _, v := range add {
-		res += v
-	}
+// 	for _, v := range add {
+// 		res += v
+// 	}
 
-	return res
-}
+// 	return res
+// }
